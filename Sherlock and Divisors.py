c909e10595fa9379d78e68ad2a163d3e9ae8c69b
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-07-31 21:52:22 +0530

    Create Sherlock and Divisors.py

---
+++ Sherlock and Divisors.py
@@ -0,0 +1,32 @@
+"""
+https://www.hackerrank.com/challenges/sherlock-and-divisors/problem
+"""
+"""
+Watson gives an integer N  to Sherlock and asks him: 
+What is the number of divisors of N that are divisible by 2?.
+"""
+
+
+
+import math
+
+
+def divisors(n):
+    count = 0
+    for i in range(1,int(math.sqrt(n))+1):
+        if n%i==0 and i%2==0:
+            # i is even
+            count+=1
+        if n%(n//i)==0 and (n//i)%2==0:
+            # n//i is even and n's factor
+            count+=1
+        if i*i==n and i%2==0:
+            # if i is sqrt reduce by 1
+            count-=1
+    return count
+
+
+n=int(input())
+for i in range(n):
+    num=int(input())
+    print(divisors(num))
